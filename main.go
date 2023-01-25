0734ff8479eadee229a33db15063973bb47d3a03
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-01-25 08:48:41 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,71 +1,73 @@
 package main
 
 import (
-	"context"
-	"log"
-	"os"
-	
+        "context"
+        "log"
+        "os"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	gogpt "github.com/sashabaranov/go-gpt3"
+        tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+        gogpt "github.com/sashabaranov/go-gpt3"
 )
 
 func main() {
-	
-       
 
-	//telegram token
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
-	
-	if err != nil {
-		log.Panic(err)
-	}
+        //telegram token
+        bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 
-	bot.Debug = true
+        if err != nil {
+                log.Panic(err)
+        }
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+        bot.Debug = true
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+        log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	updates := bot.GetUpdatesChan(u)
+        u := tgbotapi.NewUpdate(0)
+        u.Timeout = 60
 
-	for update := range updates {
-		//openai api
-		c := gogpt.NewClient(os.Getenv("OPENAI_API"))		
-		ctx := context.Background()
-		req := gogpt.CompletionRequest{
-			Model: gogpt.GPT3TextDavinci003, 
-			MaxTokens: 999,
+        updates := bot.GetUpdatesChan(u)
 
-			Prompt: update.Message.Text,
-		}
-		resp, err := c.CreateCompletion(ctx, req)
-		if err != nil {
-			return
-		}
-		if update.Message.Text == "/start" {
-			MYID := 2116777065
-			log.Printf("UserName :%s", update.Message.From.UserName)
-			log.Printf("ID :%d", update.Message.Chat.ID)
-			log.Printf("Text: %s", update.Message.Text)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hallo,Selamat datang di telegoGPT")
-			msg := tgbotapi.NewMessage(MYID, "ada user masuk.")
-			bot.Send(msg)
+        for update := range updates {
+                //openai api
 
-		} else if update.Message.Text == "/help" {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Tanyakan apa saja atau beri perintah apa saja.\n\nContoh penggunaan : \n\n-Siapa presiden indonesia pertama?\n-Buat deskripsi makanan ringan.")
-			bot.Send(msg)
+                c := gogpt.NewClient(os.Getenv("OPENAI_API"))
+                ctx := context.Background()
+                req := gogpt.CompletionRequest{
+                        Model: gogpt.GPT3TextDavinci003,
+                        MaxTokens: 999,
 
-		} else if update.Message != nil { // jika mendapat pesan
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+                        Prompt: update.Message.Text,
+                }
+                resp, err := c.CreateCompletion(ctx, req)
+                if err != nil {
+                        return
+                }
+                if update.Message.Text == "/start" {
+                        log.Printf("UserName :%s", update.Message.From.UserName)
+                        log.Printf("ID :%d", update.Message.Chat.ID)
+                        log.Printf("Text: %s", update.Message.Text)
+                        msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hallo,Selamat datang di telegoGPT")
+                        bot.Send(msg)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, resp.Choices[0].Text)
-			msg.ReplyToMessageID = update.Message.MessageID
+                } else if update.Message.Text == "/help" {
+                        msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Tanyakan apa saja atau beri perintah apa saja.\n\nContoh penggunaan : \n\n-Siapa presiden indonesia pertama?\n-Buat deskripsi makanan ringan.")
+                        bot.Send(msg)
 
-			bot.Send(msg)
-		} //
+                } else if update.Message != nil { // jika mendapat pesan
+                        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-	}
-	 
+                        //get user chat id
+                        chat, err := bot.GetChat(tgbotapi.ChatConfig{ChatID: update.Message.Chat.ID})
+                        if err != nil {
+                                log.Println(err)
+                                continue
+                        }
+                        //get user id
+                        var userId int64
+                        userId = chat.ID
+                        //send message to user
+                        msg := tgbotapi.NewMessage(userId, resp.Choices[0].Text)
+                        bot.Send(msg)
+                }
+        }
 }
