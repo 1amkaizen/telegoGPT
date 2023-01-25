b8d27af9e28650562661ea902700c27a50a8e6c6
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-01-25 08:04:11 +0000

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 fmt.Fprintln(w, "halo!")
        
 
