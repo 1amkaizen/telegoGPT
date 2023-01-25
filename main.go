c003cbbcb0ee61bf3ea01014a038f71fbd56b2c5
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-01-25 07:27:01 +0000

    Update main.go

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main() {
 			return
 		}
 		if update.Message.Text == "/start" {
+			log.Printf("hallo %s", update.Message.From.UserName)
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hallo,Selamat datang di telegoGPT")
 			bot.Send(msg)
 
@@ -58,38 +59,7 @@ func main() {
 		} //
 
 	}
-	for update := range updates {
-		//openai api
-		c := gogpt.NewClient("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")
-		ctx := context.Background()
-		req := gogpt.CompletionRequest{
-			Model:     gogpt.GPT3TextDavinci003,
-			MaxTokens: 999,
-			Prompt:    update.Message.Text,
-		}
-		resp, err := c.CreateCompletion(ctx, req)
-		if err != nil {
-			return
-		}
-		if update.Message.Text == "/start" {
-
-			log.Printf("hallo %s", update.Message.From.UserName)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hallo,Selamat datang di telegoGPT")
-			bot.Send(msg)
-
-		} else if update.Message.Text == "/help" {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Tanyakan apa saja atau beri perintah apa saja.\n\nContoh penggunaan : \n\n-Siapa presiden indonesia pertama?\n-Buat deskripsi makanan ringan.")
-			bot.Send(msg)
-
-		} else if update.Message != nil { // jika mendapat pesan
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, resp.Choices[0].Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
-		} //
+	
 
 	}
 }
