a8934cb6d8954b89d93e88bf9310527fa552f2c7
1amkaizen <1amkiz3n@gmail.com>
2023-01-28 22:03:20 +0000

    ada update kol

--- controllers/telegram-control.go
+++ controllers/telegram-control.go
@@ -11,29 +11,6 @@ import (
 	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
-var conversationContext string
-
-func AccessOpenAIAPI(prompt string) (string, error) {
-	c := gogpt.NewClient(os.Getenv("OPENAI_API"))
-	ctx := context.Background()
-
-	req := gogpt.CompletionRequest{
-		Model:            gogpt.GPT3TextDavinci003,
-		MaxTokens:        150,
-		Temperature:      0.9,
-		TopP:             1,
-		FrequencyPenalty: 0.0,
-		PresencePenalty:  0.6,
-
-		Prompt: prompt,
-	}
-	resp, err := c.CreateCompletion(ctx, req)
-	if err != nil {
-		return "", err
-	}
-	return resp.Choices[0].Text, nil
-}
-
 func SetupBot() (*tgbotapi.BotAPI, error) {
 	//telegram token
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
@@ -53,8 +30,7 @@ func SetupBot() (*tgbotapi.BotAPI, error) {
 func SendMessage(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, resp.Choices[0].Text)
-	//msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 	msg.ReplyToMessageID = update.Message.MessageID
 
 	bot.Send(msg)
