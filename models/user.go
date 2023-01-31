fa88f60d3e5af865672782f97d442e092ab40b7b
1amkaizen <1amkiz3n@gmail.com>
2023-01-31 13:38:47 +0000

    update

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type Users struct {
 	Id        int64     `gorm:"primarykey" json:"id"`
 	UserID    string    `gorm:"type:text" json:"user_id"`
 	UserName  string    `gorm:"type:varchar(300)" json:"user_name"`
-	FirstName string    `gorm:"type:varchar(300)" json:"user_name"`
-	LastName  string    `gorm:"type:varchar(300)" json:"user_name"`
+	FirstName string    `gorm:"type:varchar(300)" json:"first_name"`
+	LastName  string    `gorm:"type:varchar(300)" json:"last_name"`
 	StartDate time.Time `gorm:"type:datetime;default:current_timestamp" json:"start_date"`
 }
