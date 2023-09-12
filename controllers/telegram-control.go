64269bbad98516ddcdfb40282206d56fa09cbcfe
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-09-12 18:52:23 +0700

    Update telegram-control.go

--- controllers/telegram-control.go
+++ controllers/telegram-control.go
@@ -148,3 +148,17 @@ func HandleHelpCommand(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	bot.Send(msg)
 
 }
+
+
+func GetMessages(w http.ResponseWriter, r *http.Request) {
+    var messages []models.Messages
+    if err := models.DB.Find(&messages).Error; err != nil {
+        // Handle error
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Kirim data sebagai respons JSON
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(messages)
+}
