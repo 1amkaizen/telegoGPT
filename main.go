960cbd00c5b6e40cb95b007eb1751fd6549b6337
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-09-14 17:32:43 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-"net/http"
 	"fmt"
 	"log"
 	"os"
@@ -18,24 +17,26 @@ import (
 
 
 func main() {
-    models.ConnectDatabase()
 
-    //telegram token
-    bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	models.ConnectDatabase()
 
-    if err != nil {
-        log.Panic(err)
-        fmt.Println("MISSING_TELEGRAM_BOT_TOKEN")
-    }
+	//telegram token
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 
-    u := tgbotapi.NewUpdate(0)
-    u.Timeout = 60
+	if err != nil {
+		log.Panic(err)
+		fmt.Println("MISSING_TELEGRAM_BOT_TOKEN")
+	}
 
-    updates := bot.GetUpdatesChan(u)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-    
+	updates := bot.GetUpdatesChan(u)
 
-    for update := range updates {
+
+
+	
+	for update := range updates {
 
 		//openai api
 		if update.Message != nil { // jika mendapat pesan
@@ -76,7 +77,9 @@ func main() {
 			}
 		}
 
+	}
+	
+	
 
-
-	    
+	
 }
