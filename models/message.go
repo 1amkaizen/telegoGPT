17a6c0b57ee92220106f3b972b3bc212f2092662
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-09-17 10:37:41 +0700

    Update message.go

--- models/message.go
+++ models/message.go
@@ -3,14 +3,11 @@ package models
 import "time"
 
 type Messages struct {
-    ID        uint64    `gorm:"primaryKey;autoIncrement" json:"id"`
-    MessageID int       `gorm:"type:int" json:"message_id"`
-    UserID    string    `gorm:"type:varchar(300)" json:"user_id"`
-    Message   string    `gorm:"type:varchar(300)" json:"message"`
-    Reply     string    `gorm:"type:varchar(300)" json:"reply"`
-    UserName  string    `gorm:"type:varchar(300)" json:"user_name"`
-    CreatedAt time.Time `gorm:"type:datetime" json:"created_at"`
-
-
+	ID        uint64    `gorm:"primaryKey;autoIncrement" json:"id"`
+	MessageID int       `gorm:"type:int" json:"message_id"`
+	UserID    string    `gorm:"type:varchar(300)" json:"user_id"`
+	Message   string    `gorm:"type:varchar(300)" json:"message"`
+	Reply     string    `gorm:"type:varchar(300)" json:"reply"`
+	UserName  string    `gorm:"type:varchar(300)" json:"user_name"`
+	CreatedAt time.Time `gorm:"type:datetime;default:currentTimestamp" json:"created_at"`
 }
-
