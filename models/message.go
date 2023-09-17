ca52aa37c3b64715d2c517631103a865b212ef93
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-09-17 16:41:31 +0700

    Update message.go

--- models/message.go
+++ models/message.go
@@ -9,5 +9,5 @@ type Messages struct {
 	Message   string    `gorm:"type:varchar(300)" json:"message"`
 	Reply     string    `gorm:"type:varchar(300)" json:"reply"`
 	UserName  string    `gorm:"type:varchar(300)" json:"user_name"`
-	CreatedAt int       `gorm:"type:datetime;default:currentTimestamp" json:"created_at"`
+	CreatedAt time.Time `gorm:"type:datetime;default:currentTimestamp" json:"created_at"`
 }
