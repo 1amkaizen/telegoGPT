84c2e697fc133037906f68c394ad79accf4d953c
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-09-17 09:25:00 +0700

    Update message.go

--- models/message.go
+++ models/message.go
@@ -9,5 +9,14 @@ type Messages struct {
 	Message   string    `gorm:"type:varchar(300)" json:"message"`
 	Reply     string    `gorm:"type:varchar(300)" json:"reply"`
 	UserName  string    `gorm:"type:varchar(300)" json:"user_name"`
-	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	CreatedAt string    `gorm:"column:created_at" json:"created_at"` // Perubahan di sini
 }
+func (m *Messages) AfterFind(tx *gorm.DB) error {
+	// Konversi string ke time.Time
+	parseTime, err := time.Parse("2006-01-02 15:04:05", m.CreatedAt)
+	if err != nil {
+		return err
+	}
+	m.CreatedAt = parseTime.Format(time.RFC3339)
+	return nil
+} 
