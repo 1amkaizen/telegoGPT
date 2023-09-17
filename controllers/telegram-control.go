b62660addf2ecd55edff0d1b655b2a0e94479e48
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-09-17 16:31:14 +0700

    Update telegram-control.go

--- controllers/telegram-control.go
+++ controllers/telegram-control.go
@@ -34,7 +34,7 @@ func SaveMessageToDB(message tgbotapi.Message, reply string) {
         Message:   message.Text,
         Reply:     reply,
         UserName:  userName, 
-        CreatedAt: message.Date,
+        
     }
 
 
